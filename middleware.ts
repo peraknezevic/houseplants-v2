6dc618a94f170d7b6964db3689392c37242a68bf
Pera Knezevic <predragknezevic@icloud.com>
2023-11-20 16:18:37 +0100

    Middleware updated

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ["/dashboard/:path*", "/api/:path*"],
+  matcher: ["/dashboard/:path*", "/api/:path*", "/members/articles/:path*"],
 };
