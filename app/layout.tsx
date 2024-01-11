ef373d4a36de2ea5cf2804794a1249d33c8c6859
Pera Knezevic <predragknezevic@icloud.com>
2024-01-11 20:52:45 +0100

    Added Posthog

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { ReactNode, Suspense } from "react";
+import { PHProvider, PostHogPageview } from "./providers";
 import AuthProvider from "./auth/Provider";
 import { Analytics } from "@vercel/analytics/react";
 import { GeistSans } from "geist/font";
@@ -11,23 +13,24 @@ export const metadata: Metadata = {
   description: "All about your indoor plants",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body
-        className={`${GeistSans.className} bg-pink-50 text-emerald-950 dark:bg-emerald-950 dark:text-zinc-300`}
-      >
-        <AuthProvider>
-          <Header />
-          <div className="mx-auto my-8 max-w-4xl md:py-16">{children}</div>
-          <Analytics />
-          <Footer />
-        </AuthProvider>
-      </body>
+      <Suspense>
+        <PostHogPageview />
+      </Suspense>
+      <PHProvider>
+        <body
+          className={`${GeistSans.className} bg-pink-50 text-emerald-950 dark:bg-emerald-950 dark:text-zinc-300`}
+        >
+          <AuthProvider>
+            <Header />
+            <div className="mx-auto my-8 max-w-4xl md:py-16">{children}</div>
+            <Analytics />
+            <Footer />
+          </AuthProvider>
+        </body>
+      </PHProvider>
     </html>
   );
 }
